77449ec38212f1914acd38ba36e89893baa175b2
Sihle <sshabangu061@gmail.com>
2024-08-12 22:20:27 +0200

    updated a couple of files and added gitignore file

--- main.py
+++ main.py
@@ -2,28 +2,27 @@ import requests
 from i10data import data
 
 
-
 def detect_car_details(image_byte):
-   
+
     # API endpoint
-    url = 'https://api.carnet.ai/v2/mmg/detect'
+    url = "https://api.carnet.ai/v2/mmg/detect"
 
     # Query parameters
     params = {
-        'box_offset': 0,
-        'box_min_width': 100,
-        'box_min_height': 100,
-        'box_min_ratio': 0.50,
-        'box_max_ratio': 3.15,
-        'box_select': 'center',
-        'region': 'EU'
+        "box_offset": 0,
+        "box_min_width": 100,
+        "box_min_height": 100,
+        "box_min_ratio": 0.50,
+        "box_max_ratio": 3.15,
+        "box_select": "center",
+        "region": "EU",
     }
 
     # Headers
     headers = {
-        'accept': 'application/json',
-        'api-key': 'bfcc5c7a-3980-4e16-a7a1-b0e49a6402a6',  # Replace with # with 6
-        'Content-Type': 'application/octet-stream'
+        "accept": "application/json",
+        "api-key": "bfcc5c7a-3980-4e16-a7a1-b0e49a6402a6",  # Replace with # with 6
+        "Content-Type": "application/octet-stream",
     }
 
     # Read the image file in binary mode
@@ -34,7 +33,7 @@ def detect_car_details(image_byte):
     response = requests.post(url, params=params, headers=headers, data=image_byte)
 
     # Check if the request was successful
-    if response.status_code == 200 :
+    if response.status_code == 200:
         return refine_raw_data(response.json())  # Return the JSON response
     else:
         return response.status_code, response.text  # Return error status and message
@@ -50,6 +49,7 @@ def refine_raw_data(response):
                 if "make_id" in mmg and "make_name" in mmg:
                     return mmg
     return {"Car": "Unrecognizable"}  # Return None if no matching mmg found
+
 
 # Extract the mmg information
 
