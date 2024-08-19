35a47593b97f995842fe5f30f29c157155d760ad
kyle <kkndlovu9@gmail.com>
2024-08-19 16:14:28 +0200

    session management added

--- message_handler.py
+++ message_handler.py
@@ -14,8 +14,7 @@ takes 2 parameters the message to send and user number contry code inclueded.
 def send_message(message,contact_number):
     print(contact_number)
     print("in twilio send message")
-    # account_sid = "ACa0eee678c34990d54dabeddd258ceb45"
-    # auth_token = '8e145c3cd5ea63257bd270014894ee26'
+    
    
 
     msg = MessagingResponse()
