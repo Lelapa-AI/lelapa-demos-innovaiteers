4ce8287bea6833882ad8f6ff9cfe1ed175e48a78
Calistas Mlilo <cmlilo2004@gmail.com>
2024-10-08 19:12:51 +0000

    Work done

--- Api/transcribe.py
+++ Api/transcribe.py
@@ -1,12 +1,36 @@
 from google.cloud import speech_v1p1beta1 as speech
 from google.oauth2 import service_account
+from requests import post
+import base64
+from dotenv import load_dotenv
+import os
 
+# Load environment variables from .env file
+load_dotenv()
+VULAVULA_TOKEN = os.getenv("VULAVULA_KEY")
+VULAVULA_TRANSCRIBE_URL = "https://vulavula-services.lelapa.ai//api/v1/transcribe/sync"
 client_file = "qoutelynx-afeb607d0f34.json"
 credentials = service_account.Credentials.from_service_account_file(client_file)
 client = speech.SpeechClient(credentials=credentials)
 
 first_lang = "en-US"
 second_lang = "es"
+
+
+def speech_text(content="audios.wav"):
+    """Transcribes audio using Lelapi first if fails uses ai
+    Args:
+        content (str, optional): _description_. Defaults to "audios.wav".
+    Returns:
+        String: Returns transcribed audio either from lepai or ai
+    """
+    FILE_SIZE = os.path.getsize(content)
+    possible_trascption = speech_to_text_vula(content,FILE_SIZE)
+    if possible_trascption == {'Lelapi':"offline"}: 
+        return speech_recognition(content)
+    else:
+        return possible_trascption
+
 
 def speech_recognition(speech_file):
     try:
@@ -39,5 +63,59 @@ def speech_recognition(speech_file):
         return "Error while listening to voice note ask for a clear voice note or suggest typing"
 
 
+def speech_to_text_vula(content,file_size):
+    """Lelapi_transcrption
+
+    Args:
+        content (_type_): _description_
+        file_size (_type_): _description_
+    """
+
+    # Open file in binary mode
+    with open(content, 'rb') as file:
+        # Read file
+        file_content = file.read()
+
+    # Encode file content
+    encoded_content = base64.b64encode(file_content)
+
+    # Decode bytes to string
+    encoded_string = encoded_content.decode()
+    
+    # The req can also take the preffered language
+    transport_request_body = {
+        "file_name": content,
+        "audio_blob": encoded_string,
+        # 'language_code': 'eng',
+        "file_size": 0,
+    }
+
+    headers1 = {
+    'Content-Type': 'application/json',
+    'X-CLIENT-TOKEN': VULAVULA_TOKEN
+    }
+
+
+    resp = post(
+        VULAVULA_TRANSCRIBE_URL,
+        json=transport_request_body,
+        headers=headers1,
+    )
+   
+
+    try: 
+        response_transcribe = resp.json()
+        
+        # Access the 'text' key and check its length
+        text = response_transcribe.get('text', "")  # Safely get the 'text' key or default to empty string
+        if len(text) > 3:
+            return text
+        else:
+            return "User voice note not clear"
+    except:
+        return {'Lelapi':"offline"}
+
+
+
 if __name__ == "__main__":
-    print(speech_recognition("audios.wav"))
+    print(speech_text("about_time.wav"))
